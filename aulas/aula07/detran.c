6a78fb2d4f715ff1bfc4d8a70aa2aca8ea99a042
Phelipe <phelipe.silva@sptech.school>
2025-01-05 23:33:25 -0300

    removendo arquivos html e adicionando exercicios (não feitos)

---
+++ aulas/aula07/detran.c
@@ -0,0 +1,16 @@
+// --- Bibliotecas Iniciais ---
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Autor: Phelipe Bruione da Silva
+    Objetivo do programa: Fazer programa que mostre se a pessoa pode dirigir ou não
+    Dia do programa: __/__/2025
+*/
+
+// --- Função Principal ---
+int main()
+{
+    // --- Declaração das variáveis ---
+    
+    return 0;
+} // end main
