bb7c91ffe5391b86ff6fe1ee2b29496a8e714bf4
Phelipe <phelipe.silva@sptech.school>
2025-01-21 19:07:57 -0300

    aula14 - modificando exercício da prova

--- aulas/aula14/correcaoProvas.c
+++ aulas/aula14/correcaoProvas.c
@@ -3,14 +3,22 @@
 #include <stdlib.h>
 /*
     Autor: Phelipe Bruione da Silva
-    Objetivo do programa: Descreva o objetivo do programa
-    Dia do programa: __/__/2025
+    Objetivo do programa: Fazer um programa que leia um gabarito de uma prova ler as respostas do aluno, Ao final, mostre os acertos dos alunos e a média da turma
+    Dia do programa: 21/01/2025
 */
+#define TAM
+// --- Protótipo das Funções ---
 
 // --- Função Principal ---
 int main()
 {
-    // --- Declaração das variáveis ---
+    // --- Declaração das variáveis --- 
     
+    puts("-------------------------------");
+    puts("PASSO 1 - Cadastro do Gabarito");
+    puts("-------------------------------");
+
     return 0;
 } // end main
+
+// --- Desenvolvimento das Funções ---
