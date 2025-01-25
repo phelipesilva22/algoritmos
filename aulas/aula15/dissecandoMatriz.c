4aa3ace5389f4c0a47cb0317f85f4f6ebf43fecb
Phelipe <phelipe.silva@sptech.school>
2025-01-25 18:29:24 -0300

    aula15 - exercício dissecando matriz

---
+++ aulas/aula15/dissecandoMatriz.c
@@ -0,0 +1,23 @@
+// --- Bibliotecas Iniciais ---
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Autor: Phelipe Bruione da Silva
+    Objetivo do programa: Fazer um programa que leia uma matriz 4x4 e mostre
+    - [1] Mostrar a matriz
+    - [2] Diagonal Principal
+    - [3] Triangulo Superior
+    - [4] Triângulo Inferior
+    Dia do programa: 25/01/2025
+*/
+// --- Protótipo da Funções ---
+
+// --- Função Principal ---
+int main()
+{
+    // --- Declaração das variáveis ---
+    
+    return 0;
+} // end main
+
+// --- Desenvolvimento da Funções ---
