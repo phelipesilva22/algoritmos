7cf52ca1b13cc16e23ef74fb4e19bfc4c37d2654
Phelipe <phelipe.silva@sptech.school>
2025-01-25 22:37:39 -0300

    aula15 - continuando exercício dissecando matriz

--- aulas/aula15/dissecandoMatriz.c
+++ aulas/aula15/dissecandoMatriz.c
@@ -10,14 +10,36 @@
     - [4] Triângulo Inferior
     Dia do programa: 25/01/2025
 */
+#define TAM 4
 // --- Protótipo da Funções ---
-
+void menu();
 // --- Função Principal ---
 int main()
 {
     // --- Declaração das variáveis ---
+    int matriz[TAM][TAM], op;
+
+    do
+    {
+        menu();
+        scanf("%d", &op);
+        system("clear");
+    } while (op != 5);
+    
     
     return 0;
 } // end main
 
 // --- Desenvolvimento da Funções ---
+void menu()
+{
+    puts("=====================");
+    puts("    MENU DE OPÇÕES   ");
+    puts("=====================");
+    puts("[1] Mostrar a Matriz");
+    puts("[2] Diagonal Principal");
+    puts("[3] Triângulo Superior");
+    puts("[4] Triângulo Inferior");
+    puts("[5] Sair");
+    printf("==== OPÇÃO: ");
+} // end menu
