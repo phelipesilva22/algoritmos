a4ea870bb366d0dcbb6f1d7de66254c61ac1ae70
Phelipe <phelipe.silva@sptech.school>
2025-01-28 22:59:21 -0300

    aula15 - alterações jogo da velha

--- aulas/aula15/jogoDaVelha.c
+++ aulas/aula15/jogoDaVelha.c
@@ -6,14 +6,31 @@
     Objetivo do programa: Fazer um programa que realize um jogo da velha 
     Dia do programa: 28/01/2025
 */
+#define TAM 3
+int velha[TAM][TAM];
 // --- Protótipo das Funções ---
-
+void desenharJogoDaVelha(int v[TAM][TAM]);
 // --- Função Principal ---
 int main()
 {
     // --- Declaração das variáveis ---
     
+    desenharJogoDaVelha(velha);
+    
+
     return 0;
 } // end main
 
 // --- Desenvolvimento das Funções ---
+void desenharJogoDaVelha(int v[TAM][TAM])
+{
+    for (int l = 0; l < TAM; l++)
+    {
+        puts("+---+---+---+");
+        for (int c = 0; c < TAM; c++)
+        {
+            printf("| %d| ", l);
+        }
+        putchar('\n');
+    }
+} // end desenharJogoDaVelha
