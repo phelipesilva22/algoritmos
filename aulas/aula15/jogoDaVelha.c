3f70e8556b5ea453f12de81764a3d4acecee89ec
Phelipe <phelipe.silva@sptech.school>
2025-01-28 22:09:07 -0300

    aul15 - iniciando exercício jogo da velha

---
+++ aulas/aula15/jogoDaVelha.c
@@ -0,0 +1,19 @@
+// --- Bibliotecas Iniciais ---
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Autor: Phelipe Bruione da Silva
+    Objetivo do programa: Fazer um programa que realize um jogo da velha 
+    Dia do programa: 28/01/2025
+*/
+// --- Protótipo das Funções ---
+
+// --- Função Principal ---
+int main()
+{
+    // --- Declaração das variáveis ---
+    
+    return 0;
+} // end main
+
+// --- Desenvolvimento das Funções ---
