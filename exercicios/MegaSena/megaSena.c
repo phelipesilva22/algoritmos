2ad6abae87c756c52e0ddfdd71108bc1de587792
Phelipe <phelipe.silva@sptech.school>
2025-01-29 22:24:33 -0300

    alterações nos diretórios e criação de novos exercícios

---
+++ exercicios/MegaSena/megaSena.c
@@ -0,0 +1,26 @@
+// --- Bibliotecas Iniciais ---
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+    Autor: Phelipe Bruione da Silva
+    Objetivo do programa: Fazer um programa que simule a mega sena, onde o usuário informará os números da sua aposta e após esse procedimento, será mostrado os números sorteados.
+    Por fim, mostrará ao apostador quantos números foram acertados e se recebeu algum dinheiro:
+    - 6 NÚMEROS ACERTADOS (SENA)
+    - 5 NÚMEROS ACERTADOS (QUINA)
+    - 4 NÚMEROS ACERTADOS (QUADRA)
+    Dia do programa: 29/01/2025
+*/
+// --- Protótipo das Funções ---
+
+// --- Função Principal ---
+int main()
+{
+    // --- Declaração das variáveis ---
+    
+    puts("----------------- MEGA DA VIRADA (R$635 MILHÕES) -----------------");
+
+    return 0;
+} // end main
+
+// --- Desenvolvimento das Funções ---
