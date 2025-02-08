d04c9efa5c5b205855dfcb75d0da5626276a1c13
Phelipe <phelipe.silva@sptech.school>
2025-02-07 22:42:14 -0300

    Criando função menu do caixa

--- exercicios/simuladorBancario/simuladorBancario.c
+++ exercicios/simuladorBancario/simuladorBancario.c
@@ -1,9 +1,10 @@
 // --- Bibliotecas Iniciais ---
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /*
     Autor: Phelipe Bruione da Silva
-    Objetivo do programa: Fazer um programa qu simule um banco, tendo operações como:
+    Objetivo do programa: Fazer um programa qu simule um caixa eletrônico, tendo operações como:
     - Saque
     - Depósito Bancário
     - Consultar Saldo
@@ -11,17 +12,43 @@
     Dia do programa: 07/02/2025
 */
 // --- Protótipo das Funções ---
-
+void menuInicial();
 // --- Função Principal ---
 int main()
 {
     // --- Declaração das variáveis ---
+    char respUser;
+    unsigned short op;
+
+    do
+    {
+        menuInicial();
+        scanf("%hu", &op);
+
+
+        
+
+
+        printf("Deseja continuar? [S/N] ");
+        scanf(" %c", &respUser);
+        respUser = tolower(respUser);
+        system("clear");
+    } while (respUser != 'n');
     
-    puts("------------------- BANCO ITAÚ -------------------");
-    puts("          O BANCO PARA VOCÊ E SUA FAMÍLIA         ");
-    puts("--------------------------------------------------");
+    
 
     return 0;
 } // end main
 
 // --- Desenvolvimento das Funções ---
+void menuInicial()
+{
+    puts("------------------- BANCO ITAÚ -------------------");
+    puts("          O BANCO PARA VOCÊ E SUA FAMÍLIA         ");
+    puts("--------------------------------------------------");
+    puts("[1] - SAQUE");
+    puts("[2] - DEPÓSITO BANCÁRIO");
+    puts("[3] - CONSULTAR SALDO");
+    puts("[4] - EXTRATO BANCÁRIO");
+    printf("Selecione sua opção: ");
+} // end menuInicial
