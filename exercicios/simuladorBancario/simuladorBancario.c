678c4ec1a72cc0d058f16e011fc1bd3440c6f390
Phelipe <phelipe.silva@sptech.school>
2025-02-07 21:14:26 -0300

    estrutura inicial do  exercício simulador bancario

---
+++ exercicios/simuladorBancario/simuladorBancario.c
@@ -0,0 +1,27 @@
+// --- Bibliotecas Iniciais ---
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Autor: Phelipe Bruione da Silva
+    Objetivo do programa: Fazer um programa qu simule um banco, tendo operações como:
+    - Saque
+    - Depósito Bancário
+    - Consultar Saldo
+    - Extrato Bancário
+    Dia do programa: 07/02/2025
+*/
+// --- Protótipo das Funções ---
+
+// --- Função Principal ---
+int main()
+{
+    // --- Declaração das variáveis ---
+    
+    puts("------------------- BANCO ITAÚ -------------------");
+    puts("          O BANCO PARA VOCÊ E SUA FAMÍLIA         ");
+    puts("--------------------------------------------------");
+
+    return 0;
+} // end main
+
+// --- Desenvolvimento das Funções ---
