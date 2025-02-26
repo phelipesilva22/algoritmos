d2078d3899d76a0078db40fe806623fe02ba8a4d
Phelipe <phelipe.silva@sptech.school>
2025-02-26 14:08:42 -0300

    update - feita parte do celsius

--- exercicios/conversorTemperatura/conversorTemperatura.c
+++ exercicios/conversorTemperatura/conversorTemperatura.c
@@ -10,14 +10,19 @@
     [3] - KELVIN 
     Dia do programa: 26/02/2025
 */
+// --- Variáveis Globais ---
+float celsius;
 // --- Prótotipo das Funções ---
 void menuConversor();
+void linha();
+float leituraCelsius();
 // --- Função Principal ---
 int main()
 {
     // --- Declaração das variáveis ---
     char res;
     unsigned short op;
+    float C;
 
     do
     {
@@ -25,23 +30,25 @@ int main()
         menuConversor();
         scanf("%hu", &op);
 
-
         switch (op)
         {
         case 1:
-            /* code */
+            linha();
+            C = leituraCelsius();
+            printf("CELSIUS -> FAHRENHEIT: %.2fCº = %.1fF!\n", C, (C * 1.8 + 32));
+            printf("CELSIUS -> KELVIN: %.2fCº = %.1fK!\n", C, (C + 273));
+            linha();
             break;
-        
         default:
+            puts("OPÇÃO INVÁLIDA!!");
             break;
         }
-
 
         printf("Deseja continuar? [S/N] ");
         scanf(" %c", &res);
         res = tolower(res);
     } while (res != 'n');
-    
+    puts("FINALIZANDO O PROGRAMA...");
 
     return 0;
 } // end main
@@ -54,4 +61,17 @@ void menuConversor()
     puts("[2] - FAHRENHEIT");
     puts("[3] - KELVIN");
     printf("Qual temperatura você quer converter? ");
-}
+} // end menuConversor
+
+void linha()
+{
+    puts("-------------------------------------------");
+} // end linha
+
+float leituraCelsius()
+{
+    printf("Informe o valor da temperatura em Cº: ");
+    scanf("%f", &celsius);
+
+    return celsius;
+} // end leituraCelsius
